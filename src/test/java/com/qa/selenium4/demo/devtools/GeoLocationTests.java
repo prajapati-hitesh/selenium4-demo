80e4342fad88d37957b96ce6081ad50e3ec08a0a
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2021-01-25 23:24:38 +0530

    GeoLocationTests: Test Cases added for Mocking Geo Location

---
+++ src/test/java/com/qa/selenium4/demo/devtools/GeoLocationTests.java
@@ -0,0 +1,80 @@
+package com.qa.selenium4.demo.devtools;
+
+import com.qa.selenium4.demo.base.BaseDriver;
+import com.qa.selenium4.demo.helper.WaitHelper;
+import com.qa.selenium4.demo.pages.whatsmylocation.WhatsMyLocationPage;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v87.emulation.Emulation;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+public class GeoLocationTests extends BaseDriver {
+
+    @Test(priority = 0, description = "Check Current Geo-Location")
+    public void geoLocationTest() {
+        // Load URL
+        driver.get("https://whatmylocation.com/");
+
+        // Wait for 5 seconds
+        WaitHelper.hardWait(5);
+
+        WhatsMyLocationPage whatsMyLocationPage = new WhatsMyLocationPage(driver);
+
+        // Move To Map
+        whatsMyLocationPage.moveToMap();
+
+        // Get Address Details
+        whatsMyLocationPage
+                .getAddressDetails()
+                .forEach((key, value) -> {
+                    System.out.println(key + " : " + value);
+                });
+    }
+
+    @Test(priority = 1, description = "Mock geo-location using Chrome Dev Tools", dataProvider = "getLatLong")
+    public void mockGeoLocationUsingCDPTest(String location, Number latitude, Number longitude, Number accuracy) {
+        // Get Chrome Dev Tools
+        DevTools chromeDevTools = getDevTools();
+
+        // Create Session
+        chromeDevTools.createSession();
+
+        // Emulate Geo Location
+        chromeDevTools.send(Emulation.setGeolocationOverride(
+                Optional.of(latitude),
+                Optional.of(longitude),
+                Optional.of(accuracy)
+        ));
+
+        // Load URL
+        driver.get("https://whatmylocation.com/");
+
+        // Wait for 5 seconds
+        WaitHelper.hardWait(5);
+
+        WhatsMyLocationPage whatsMyLocationPage = new WhatsMyLocationPage(driver);
+
+        // Move To Map
+        whatsMyLocationPage.moveToMap();
+
+        System.out.println("\nLocation Details For : " + location);
+
+        // Get Address Details
+        whatsMyLocationPage
+                .getAddressDetails()
+                .forEach((key, value) -> {
+                    System.out.println(key + " : " + value);
+                });
+    }
+
+    @DataProvider(name = "getLatLong")
+    public Object[][] getLatLong() {
+        return new Object[][]{
+                {"New York, US", 40.712776, -74.005974, 100},
+                {"Leeds, UK", 53.800755, -1.549077, 100},
+                {"Eiffel Tower, Paris", 48.858372, 2.294481, 100}
+        };
+    }
+}
