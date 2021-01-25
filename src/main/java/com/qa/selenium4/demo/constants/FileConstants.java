2b5a45ad7f2273118f46147c1cc03c28f41fce31
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2021-01-25 23:24:17 +0530

    FileConstants: Constant for Chrome Extension location added.

--- src/main/java/com/qa/selenium4/demo/constants/FileConstants.java
+++ src/main/java/com/qa/selenium4/demo/constants/FileConstants.java
@@ -6,4 +6,5 @@ public class FileConstants {
     public static final String USER_DIR = SystemUtility.getUserDirectory();
     public static final String FILE_SEPARATOR = SystemUtility.getFileSeparator();
     public static final String SCREENSHOT_ROOT_DIR = USER_DIR + FILE_SEPARATOR + "screenshot";
+    public static final String CHROME_EXTENSION_DIR = USER_DIR + FILE_SEPARATOR + "chrome-extension" + FILE_SEPARATOR;
 }
