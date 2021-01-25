cb0ae8e9c576436ccd42b99a0a4fdec190c5f086
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2021-01-25 15:45:39 +0530

    WaitHelper: helper created to add wrapper on top of Implicit, explicit and hard waits.

---
+++ src/main/java/com/qa/selenium4/demo/helper/WaitHelper.java
@@ -0,0 +1,21 @@
+package com.qa.selenium4.demo.helper;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitHelper {
+
+    public static WebDriverWait getWaitObject(WebDriver driver, Duration duration) {
+        return new WebDriverWait(driver, duration);
+    }
+
+    public static WebElement waitAndGetElement(WebDriver driver, Duration duration, By by) {
+        return WaitHelper.getWaitObject(driver, duration)
+                .until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+}
