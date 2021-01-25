404e6560d30e975f5171cb8b85d99551c567b246
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2021-01-25 23:22:53 +0530

    ActionHelper: Helper class created for Selenium's action class

---
+++ src/main/java/com/qa/selenium4/demo/helper/ActionHelper.java
@@ -0,0 +1,23 @@
+package com.qa.selenium4.demo.helper;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionHelper {
+
+    public static void moveToElement(WebDriver driver, By by) {
+        new Actions(driver)
+                .moveToElement(driver.findElement(by))
+                .build()
+                .perform();
+    }
+
+    public static void moveToElement(WebDriver driver, WebElement element) {
+        new Actions(driver)
+                .moveToElement(element)
+                .build()
+                .perform();
+    }
+}
