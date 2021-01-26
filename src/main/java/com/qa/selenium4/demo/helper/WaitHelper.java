35a07b13ddc543504d8ce11b5e12d239b8fb769c
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2021-01-26 15:02:28 +0530

    WaitHelper: waitUntil Visibility of element and text available static function added

--- src/main/java/com/qa/selenium4/demo/helper/WaitHelper.java
+++ src/main/java/com/qa/selenium4/demo/helper/WaitHelper.java
@@ -19,6 +19,16 @@ public class WaitHelper {
                 .until(ExpectedConditions.visibilityOfElementLocated(by));
     }
 
+    public static WebElement waitUntilElementIsVisible(WebDriver driver, Duration duration, By by) {
+        return WaitHelper.getWaitObject(driver, duration)
+                .until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    public static boolean waitUntilElementHasText(WebDriver driver, Duration duration, By by, String textToBe) {
+        return WaitHelper.getWaitObject(driver, duration)
+                .until(ExpectedConditions.textToBePresentInElementLocated(by, textToBe));
+    }
+
     public static void hardWait(int timeInSeconds) {
         try {
             Thread.sleep(timeInSeconds * 1000);
