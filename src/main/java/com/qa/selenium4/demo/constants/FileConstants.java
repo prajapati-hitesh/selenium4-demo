e5d4db22a4369d4b445d4f87cb498b4d74c5bc43
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2021-01-28 15:14:14 +0530

    FileConstants: Screenshot root dir path updated

--- src/main/java/com/qa/selenium4/demo/constants/FileConstants.java
+++ src/main/java/com/qa/selenium4/demo/constants/FileConstants.java
@@ -5,6 +5,6 @@ import com.qa.selenium4.utils.SystemUtility;
 public class FileConstants {
     public static final String USER_DIR = SystemUtility.getUserDirectory();
     public static final String FILE_SEPARATOR = SystemUtility.getFileSeparator();
-    public static final String SCREENSHOT_ROOT_DIR = USER_DIR + FILE_SEPARATOR + "screenshot";
+    public static final String SCREENSHOT_ROOT_DIR = USER_DIR + FILE_SEPARATOR + "screenshot" + FILE_SEPARATOR;
     public static final String CHROME_EXTENSION_DIR = USER_DIR + FILE_SEPARATOR + "chrome-extension" + FILE_SEPARATOR;
 }
