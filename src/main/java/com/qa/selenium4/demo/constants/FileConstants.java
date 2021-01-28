22b5d679213ed11d6e5b023eaddde7f371b68d9e
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2021-01-28 18:12:58 +0530

    FileConstants: TEST Data directory constant added.

--- src/main/java/com/qa/selenium4/demo/constants/FileConstants.java
+++ src/main/java/com/qa/selenium4/demo/constants/FileConstants.java
@@ -7,4 +7,5 @@ public class FileConstants {
     public static final String FILE_SEPARATOR = SystemUtility.getFileSeparator();
     public static final String SCREENSHOT_ROOT_DIR = USER_DIR + FILE_SEPARATOR + "screenshot" + FILE_SEPARATOR;
     public static final String CHROME_EXTENSION_DIR = USER_DIR + FILE_SEPARATOR + "chrome-extension" + FILE_SEPARATOR;
+    public static final String TEST_DATA_DIR = USER_DIR + FILE_SEPARATOR + "test-data" + FILE_SEPARATOR;
 }
