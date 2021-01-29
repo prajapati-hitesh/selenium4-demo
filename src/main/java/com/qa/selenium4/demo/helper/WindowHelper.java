a10242ad10ae36b4486f0b541272662dd3a3a5b3
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2021-01-29 23:51:24 +0530

    WindowHelper: function added to get FullPageHeight using Js added.

--- src/main/java/com/qa/selenium4/demo/helper/WindowHelper.java
+++ src/main/java/com/qa/selenium4/demo/helper/WindowHelper.java
@@ -5,11 +5,18 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 
 public class WindowHelper {
-    private static Dimension getWindowSize(WebDriver driver) {
+    public static Dimension getWindowSize(WebDriver driver) {
         return driver.manage().window().getSize();
     }
 
     public static Dimension getWebPageSize(WebDriver driver) {
         return driver.findElement(By.tagName("body")).getSize();
+    }
+
+    public static double getFullPageHeight(WebDriver driver) {
+        return Double.parseDouble(JavaScriptHelper
+                .getJsExecutor(driver)
+                .executeScript("return document.body.scrollHeight;").toString()
+        );
     }
 }
