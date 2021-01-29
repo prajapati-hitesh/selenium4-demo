bea66539edda947971a97c9c696c092ff79376ba
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2021-01-29 18:23:40 +0530

    RelativeLocatorTests: Additional Relative Locator test added.

--- src/test/java/com/qa/selenium4/demo/relativelocators/RelativeLocatorTests.java
+++ src/test/java/com/qa/selenium4/demo/relativelocators/RelativeLocatorTests.java
@@ -1,15 +1,19 @@
 package com.qa.selenium4.demo.relativelocators;
 
 import com.qa.selenium4.demo.base.BaseDriver;
+import com.qa.selenium4.demo.helper.BoundingRectHelper;
 import com.qa.selenium4.demo.helper.ElementHelper;
 import com.qa.selenium4.demo.helper.JavaScriptHelper;
+import com.qa.selenium4.demo.helper.WaitHelper;
 import com.qa.selenium4.demo.pages.saucedemo.SauceDemoInventoryPage;
 import com.qa.selenium4.demo.pages.saucedemo.SauceDemoLoginPage;
 import com.qa.selenium4.demo.pages.seleniumeasy.RegisterPage;
 import org.apache.commons.lang3.time.StopWatch;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.locators.RelativeLocator;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -63,5 +67,52 @@ public class RelativeLocatorTests extends BaseDriver {
         WebElement cardElement = SauceDemoInventoryPage.backPackCardElement(driver);
 
         JavaScriptHelper.highlightElement(driver, cardElement);
+    }
+
+    @Test(priority = 2, description = "Locate elements using RelativeLocator strategy. Bad Example")
+    public void relativeLocatorLoginPageBadExampleTest() {
+        // Load URL
+        driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+
+        // Get "Email Address" input element
+        WebElement emailInputElement = driver.findElement(RelativeLocator
+                .withTagName("input")
+                .above(By.id("passwd"))
+                .toRightOf(By.id("email_create"))
+        );
+
+        // Highlight element and
+        JavaScriptHelper.highlightElement(driver, emailInputElement);
+
+        // Wait for 5 sec
+        WaitHelper.hardWait(5);
+
+        // Enter Email Address
+        ElementHelper.sendKeys(driver, emailInputElement, "hiteshprajapati1992@gmail.com");
+    }
+
+    @Test(priority = 3, description = "Locate elements using RelativeLocator strategy. Good Example")
+    public void relativeLocatorLoginPageGoodExampleTest() {
+        // Load URL
+        driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+
+        // Get "Email Address" input element
+        WebElement emailInputElement = driver.findElement(RelativeLocator
+                .withTagName("input")
+                .above(By.id("passwd"))
+                .toRightOf(By.id("email_create"))
+                .below(By.id("search_query_top"))
+        );
+
+        // Highlight element and
+        JavaScriptHelper.highlightElement(driver, emailInputElement);
+
+        // Wait for 5 sec
+        WaitHelper.hardWait(5);
+
+        BoundingRectHelper boundingRectHelper = new BoundingRectHelper(driver, driver.findElement(By.id("passwd")));
+
+        // Enter Email Address
+        ElementHelper.sendKeys(driver, emailInputElement, "hiteshprajapati1992@gmail.com");
     }
 }
